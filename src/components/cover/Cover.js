34bee668d0dcd69e11a6c566b6add28bf6cae646
Andrey <forestx24@gmail.com>
2021-09-03 16:02:08 +0200

    first step

---
+++ src/components/cover/Cover.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Cover.css';
+
+const Cover = () => {
+    return (
+        <div className="cover__container">
+
+        </div>
+    );
+}
+
+export default Cover;
